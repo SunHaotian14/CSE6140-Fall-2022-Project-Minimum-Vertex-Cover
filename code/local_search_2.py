5f0fe25c7c7b06f646924890bd5588bb88b150f9
Zifeng <lzf142857@126.com>
2022-12-01 15:24:27 -0500

    Update local_search_2.py

--- code/local_search_2.py
+++ code/local_search_2.py
@@ -5,10 +5,9 @@ import heapq
 
 def local_search_2(graph, seed, cutoff_time):
 
-    def check_cover(graph_test, graph):
-        nodes_covered = graph_test.get_vertices_set()
+    def check_cover(covered_nodes, graph):
         for edge in graph.get_edges_set():
-            if (edge[0] not in nodes_covered) and (edge[1] not in nodes_covered):
+            if covered_nodes[edge[0]-1] + covered_nodes[edge[1]-1] == 0: # Neither of the two ends is covered
                 return False
         return True
 
@@ -20,23 +19,30 @@ def local_search_2(graph, seed, cutoff_time):
 
     trace = []
     start_time = time.time()
+    nodes_left = graph.get_num_nodes()
 
-    while (time.time() - start_time) < cutoff_time:
-        trace.append((time.time()-start_time, operation_graph.get_num_nodes()))
+    covered_nodes = np.full(graph.get_num_nodes(),1, dtype=int)
+
+    while(True):
+        trace.append((time.time()-start_time, np.sum(covered_nodes)))
 
         # every iteration, delete the smallest possible degree node 
         while(True):
+            print(f"Nodes left: {nodes_left}")
+            nodes_left -= 1
+
             if len(heap)==0:
                 return operation_graph.get_vertices_set(), trace
-            graph_test = operation_graph.copy()
-            min_node = heapq.heappop(heap)
-            graph_test.delete_nodes_from_set([min_node[1]]) # try to delete the node with min degree
-            if check_cover(graph_test, graph):
-                operation_graph.delete_nodes_from_set([min_node[1]])
+            min_node = heapq.heappop(heap)[1]
+            covered_nodes[min_node-1] = 0
+
+            if check_cover(covered_nodes, graph):
+                operation_graph.delete_nodes_from_set([min_node])
                 break
             else: 
+                covered_nodes[min_node-1] = 1 # set the node back to covered
                 continue
 
 
-#G = load_graph('dummy2')
-#print(local_search_2(G,1,1000))
+#G = load_graph('star.graph')
+#print(local_search_2(G,1,1))
