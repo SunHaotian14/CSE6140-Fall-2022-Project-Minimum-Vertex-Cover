b7522d2afeb6647d3c031b2703c29d47f85a2c89
Zifeng <lzf142857@126.com>
2022-12-03 18:56:21 -0500

    ls2 Randomized Hill Climbing Completed

--- code/local_search_2.py
+++ code/local_search_2.py
@@ -20,34 +20,44 @@ def local_search_2(graph, initialize_ratio, seed, cutoff_time):
     np.random.seed(seed)
     nodes = graph.get_vertices_set()
     nodes_num = graph.get_num_nodes()
-    operation_graph = graph.copy() # operations of the graph are done on a copy of the graph
 
-    covered_nodes = np.zeros(nodes_num, dtype=int)
-    covered_nodes[:int(initialize_ratio*nodes_num)] = 1
-    np.random.shuffle(covered_nodes) # initialization
+    max_init_iter = 1000
+    i = 0
+    # Try initialization for max iter times
+    while(True):
+        covered_nodes = np.zeros(nodes_num, dtype=int)+1
+        covered_nodes[:int(initialize_ratio*nodes_num)] = 0
+        np.random.shuffle(covered_nodes) # initialization, set some of the nodes to be 0
+
+        if check_cover(covered_nodes, graph):
+            break
+        i += 1
+        if i == max_init_iter:
+            print("Initialization Error!")
+            return None
     
-    heap = [(len(operation_graph.get_neighbors_as_list(node)), node) for node in nodes] # create heap
-    heapq._heapify_max(heap)
+    heap = [(len(graph.get_neighbors_as_list(node)), node) for node in nodes] # create heap, degree as key
+    heapq.heapify(heap)
 
     trace = []
     start_time = time.time()
 
     while((time.time() - start_time) < cutoff_time):
-        # every iteration, add the node with highest degree
+        # every iteration, delete the node with lowest degree
         if len(heap)==0:
-            print("Heap empty")
+            #print("Heap empty")
             return convert_to_set(covered_nodes), trace
-        max_node = heapq._heappop_max(heap)[1]
-        if covered_nodes[max_node-1] == 1:
+        min_node = heapq.heappop(heap)[1]
+        if covered_nodes[min_node-1] == 0:
             continue
-        covered_nodes[max_node-1] = 1
+        covered_nodes[min_node-1] = 0
 
-        if check_cover(covered_nodes, graph):
-            print("Finished Searching")
-            return convert_to_set(covered_nodes), trace
+        if not check_cover(covered_nodes, graph):
+            covered_nodes[min_node-1] = 1 #restore
         trace.append((time.time()-start_time, np.sum(covered_nodes)))
         #print(np.sum(covered_nodes))
+    print("Timeout!")
     return convert_to_set(covered_nodes), trace
 
-#G = load_graph('star.graph')
-#print(local_search_2(G,0.1,1,1000))
+#G = load_graph('football.graph')
+#print(local_search_2(G,0.02,1,1000))
